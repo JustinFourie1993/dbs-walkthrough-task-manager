c7dc719178a672ac50980c25f6d2754a2ca56c5d
JustinFourie1993 <>
2023-09-25 09:52:27 +0000

    Add edit_category to update tasks in db

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from taskmanager import app, db #noqa
+from taskmanager import app, db
 from taskmanager.models import Category, Task
 
 
@@ -14,7 +14,7 @@ def categories():
     return render_template("categories.html", categories=categories)
 
 
-@app.route("/add_category", methods=["get","post"])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
@@ -22,3 +22,13 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
